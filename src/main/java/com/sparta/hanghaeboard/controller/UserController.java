163ebcc9102905f02137c6851bd6b12033db603b
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-15 04:47:49 +0900

    restart

--- src/main/java/com/sparta/hanghaeboard/controller/UserController.java
+++ src/main/java/com/sparta/hanghaeboard/controller/UserController.java
@@ -1,4 +1,51 @@
 package com.sparta.hanghaeboard.controller;
 
+import com.sparta.hanghaeboard.dto.LoginRequestDto;
+import com.sparta.hanghaeboard.dto.SignupRequestDto;
+import com.sparta.hanghaeboard.dto.StatusResponseDto;
+import com.sparta.hanghaeboard.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
+
+//    @GetMapping("/signup")
+//    public ModelAndView signupPage(){
+//        return new ModelAndView("signup");
+//    }
+//
+//    @GetMapping("/login")
+//    public ModelAndView loginPage(){
+//        return new ModelAndView("login");
+//    }
+
+    @ResponseBody
+    @PostMapping("/signup")
+    public ResponseEntity<StatusResponseDto> signup(@Valid @RequestBody SignupRequestDto signupRequestDto, BindingResult result){
+        if(result.hasErrors()){
+            return ResponseEntity.badRequest().body(new StatusResponseDto(HttpStatus.BAD_REQUEST.value(), result.getFieldError().getDefaultMessage()));
+        }
+        return userService.signup(signupRequestDto);
+    }
+
+    @ResponseBody
+    @PostMapping("/login")
+    public ResponseEntity<StatusResponseDto> login(@RequestBody LoginRequestDto loginRequestDto){
+
+        return userService.login(loginRequestDto);
+    }
 }
+
--- src/main/java/com/sparta/hanghaeboard/repository/UserRepository.java
+++ src/main/java/com/sparta/hanghaeboard/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
