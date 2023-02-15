8ced0a0659b78506147d83a2243db86509d3e711
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-16 02:00:48 +0900

    fix:코드 정리

--- src/main/java/com/sparta/hanghaeboard/repository/BoardRepository.java
+++ src/main/java/com/sparta/hanghaeboard/repository/BoardRepository.java
@@ -3,7 +3,5 @@ package com.sparta.hanghaeboard.repository;
 import com.sparta.hanghaeboard.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
