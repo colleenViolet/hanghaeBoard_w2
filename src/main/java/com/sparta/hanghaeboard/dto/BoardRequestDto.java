d6d5528f7a18a12f917b80f7c8ec8218cfe1c732
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-15 15:46:51 +0900

    fix:All part

--- src/main/java/com/sparta/hanghaeboard/dto/BoardRequestDto.java
+++ src/main/java/com/sparta/hanghaeboard/dto/BoardRequestDto.java
@@ -1,10 +1,14 @@
 package com.sparta.hanghaeboard.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardRequestDto {
     private  String title;
     private String content;
