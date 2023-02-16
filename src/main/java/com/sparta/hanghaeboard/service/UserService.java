614cdbaf171a6c566489dde51e403eb2454cfdcf
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-16 23:56:55 +0900

    fix:코드 정리

--- src/main/java/com/sparta/hanghaeboard/service/UserService.java
+++ src/main/java/com/sparta/hanghaeboard/service/UserService.java
@@ -7,9 +7,6 @@ import com.sparta.hanghaeboard.entity.User;
 import com.sparta.hanghaeboard.jwt.JwtUtil;
 import com.sparta.hanghaeboard.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
